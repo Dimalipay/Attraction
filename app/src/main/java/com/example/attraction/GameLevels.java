b2748829e73d01558ed794307f0b4b7375fe8b49
lipay <lipay.dima@gmail.com>
2022-10-25 15:41:24 +0300

    CardView no  RecyclerView

---
+++ app/src/main/java/com/example/attraction/GameLevels.java
@@ -0,0 +1,66 @@
+package com.example.attraction;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.MenuItem;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelavels);
+
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        Button button_back = (Button) findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // конструкция try - catch - это ловитель ошибок
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class); //есть намерение перейти с Gamelevels в окно MainActivity
+                    startActivity(intent);
+                    finish(); //запуск процедуры перехода и команда finish закрывает текущее окно
+                } catch (Exception e) {
+
+                }
+            }
+        });
+    }
+
+    //Системная кнопка "Назад" - начало
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class); //есть намерение перейти с Gamelevels в окно MainActivity
+            startActivity(intent); finish(); //запуск процедуры перехода и команда finish закрывает текущее окно
+        }catch (Exception e){
+        }
+    }
+    //Системная кнопка "Назад" - конец
+}
---
+++ app/src/main/java/com/example/attraction/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.attraction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private long backPressedTime; //создали переменную для системной кнопки назад
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.btn_start);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, GameLevels.class);
+                    startActivity(intent);finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        Button button_exit = (Button)findViewById(R.id.button_exit);
+
+        button_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    finish();
+                    System.exit(0);
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    //Системная кнопка "Назад" - начало
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Нажмите ещё раз, что бы выйти", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+    //Системная кнопка "Назад" - конец
+}
